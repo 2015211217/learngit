95bd9539ecc641e14e016d3028267f7dbbc1a532
JiangXuanke <15223370366@163.com>
2018-08-17 10:22:53 +0800

    first commit.

---
+++ Lesson1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Lesson1
+//
+//  Created by JiangXuanke on 2018/8/17.
+//  Copyright © 2018年 JiangXuanke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
